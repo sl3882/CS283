08e559ecb909b8b4cc9577f0103d689801fd1fc8
scott <lsy@gmail.com>
2025-02-04 15:23:43 -0500

    fix

--- 3-ShellP1/starter/dshlib.c
+++ 3-ShellP1/starter/dshlib.c
@@ -32,62 +32,103 @@
  *  Standard Library Functions You Might Want To Consider Using
  *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
  */
-int build_cmd_list(char *cmd_line, command_list_t *clist)
-{
-    // Initialize the command list structure
+
+int build_cmd_list(char *cmd_line, command_list_t *clist) {
     memset(clist, 0, sizeof(command_list_t));
+    
+    // Check for empty command line
+    if (!cmd_line || strlen(cmd_line) == 0) {
+        printf(CMD_WARN_NO_CMD);
+        return WARN_NO_CMDS;
+    }
 
-    // Temporary buffer to work with (copy since strtok modifies the string)
+    // Skip leading spaces in the initial command line
+    while (*cmd_line && isspace((unsigned char)*cmd_line)) {
+        cmd_line++;
+    }
+
+    if (*cmd_line == '\0') {
+        printf(CMD_WARN_NO_CMD);
+        return WARN_NO_CMDS;
+    }
+
     char cmd_copy[SH_CMD_MAX];
-    strncpy(cmd_copy, cmd_line, SH_CMD_MAX);
-    cmd_copy[SH_CMD_MAX - 1] = '\0'; // Ensure null termination
+    strncpy(cmd_copy, cmd_line, SH_CMD_MAX - 1);
+    cmd_copy[SH_CMD_MAX - 1] = '\0';
 
-    // Tokenize the command line by pipes '|'
-    char *cmd = strtok(cmd_copy, PIPE_STRING);
-    while (cmd != NULL)
-    {
-        // Trim leading spaces
-        while (*cmd == SPACE_CHAR) cmd++;
-
-        // Ensure the number of commands does not exceed CMD_MAX
-        if (clist->num >= CMD_MAX)
-        {
+    char *cmd_ptr = cmd_copy;
+    char *next_cmd = cmd_ptr;
+    
+    while (next_cmd && *next_cmd) {
+        if (clist->num >= CMD_MAX) {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
             return ERR_TOO_MANY_COMMANDS;
         }
 
-        // Extract executable and arguments
-        command_t *curr_cmd = &clist->commands[clist->num];
-        char *exe = strtok(cmd, " ");
-        if (!exe)
-        {
-            return WARN_NO_CMDS;
+        // Find next pipe
+        char *pipe = strchr(next_cmd, PIPE_CHAR);
+        if (pipe) {
+            *pipe = '\0';
         }
 
-        // Ensure executable size does not exceed EXE_MAX
-        if (strlen(exe) >= EXE_MAX)
-        {
-            return ERR_CMD_OR_ARGS_TOO_BIG;
+        // Skip leading spaces
+        while (*next_cmd && isspace((unsigned char)*next_cmd)) {
+            next_cmd++;
         }
-        strcpy(curr_cmd->exe, exe);
 
-        // Extract arguments (everything after the first token)
-        char *args = strtok(NULL, "");
-        if (args)
-        {
-            // Ensure argument size does not exceed ARG_MAX
-            if (strlen(args) >= ARG_MAX)
-            {
-                return ERR_CMD_OR_ARGS_TOO_BIG;
+        if (*next_cmd) {
+            char *space = strchr(next_cmd, SPACE_CHAR);
+            
+            if (space) {
+                *space = '\0';
+                // Skip leading spaces in arguments
+                char *args = space + 1;
+                while (*args && isspace((unsigned char)*args)) {
+                    args++;
+                }
+                
+                // Remove trailing spaces from arguments
+                char *end = args + strlen(args) - 1;
+                while (end > args && isspace((unsigned char)*end)) {
+                    *end = '\0';
+                    end--;
+                }
+
+                if (strlen(next_cmd) >= EXE_MAX || strlen(args) >= ARG_MAX) {
+                    return ERR_CMD_OR_ARGS_TOO_BIG;
+                }
+                
+                strcpy(clist->commands[clist->num].exe, next_cmd);
+                strcpy(clist->commands[clist->num].args, args);
+            } else {
+                // Remove trailing spaces from command
+                char *end = next_cmd + strlen(next_cmd) - 1;
+                while (end > next_cmd && isspace((unsigned char)*end)) {
+                    *end = '\0';
+                    end--;
+                }
+
+                if (strlen(next_cmd) >= EXE_MAX) {
+                    return ERR_CMD_OR_ARGS_TOO_BIG;
+                }
+                
+                strcpy(clist->commands[clist->num].exe, next_cmd);
+                clist->commands[clist->num].args[0] = '\0';
             }
-            strcpy(curr_cmd->args, args);
-        }
-        else
-        {
-            curr_cmd->args[0] = '\0'; // No arguments case
+            
+            clist->num++;
         }
 
-        clist->num++;
-        cmd = strtok(NULL, PIPE_STRING);
+        if (pipe) {
+            next_cmd = pipe + 1;
+        } else {
+            break;
+        }
+    }
+
+    if (clist->num == 0) {
+        printf(CMD_WARN_NO_CMD);
+        return WARN_NO_CMDS;
     }
 
     return OK;
