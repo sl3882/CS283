2f56872d1cc9ab2f7983a16c5469522726f0647e
scott <lsy@gmail.com>
2025-02-04 14:54:21 -0500

    1st commit

--- 3-ShellP1/starter/dsh_cli.c
+++ 3-ShellP1/starter/dsh_cli.c
@@ -50,15 +50,35 @@ int main()
     int rc = 0;
     command_list_t clist;
 
-    while(1){
-        printf("%s", SH_PROMPT);
-        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
-        printf("\n");
-        break;
-        }
-        //remove the trailing \n from cmd_buff
-        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
-
-        //IMPLEMENT THE REST OF THE REQUIREMENTS
+   while(1){
+    printf("%s", SH_PROMPT);
+    if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
+    printf("\n");
+    break;
     }
+    //remove the trailing \n from cmd_buff
+    cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
+    
+    if(strcmp(*cmd_buff,EXIT_CMD)==0){
+        break;
+    }
+    rc = build_cmd_list(cmd_buff, &clist);
+    if(rc == OK){
+        printf(CMD_OK_HEADER,clist.num);
+        for (int i = 0; i < clist.num; i++)
+        {
+            printf("Command %d: %s %s\n", i + 1, clist.commands[i].exe, clist.commands[i].args);
+        }
+        
+    }
+    else if(rc == WARN_NO_CMDS){
+        printf(CMD_WARN_NO_CMD);
+    }
+    else if (rc == ERR_TOO_MANY_COMMANDS ){
+        printf(CMD_ERR_PIPE_LIMIT,CMD_MAX);
+    }
+    
+
+    }
+    return 0;
 }
