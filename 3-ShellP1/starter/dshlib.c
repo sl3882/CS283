ee2fafec8a3cbdd84d1ac21733c21f0872c0560a
scott <lsy@gmail.com>
2025-02-04 15:17:34 -0500

    test

--- 3-ShellP1/starter/dshlib.c
+++ 3-ShellP1/starter/dshlib.c
@@ -34,6 +34,61 @@
  */
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+    // Initialize the command list structure
+    memset(clist, 0, sizeof(command_list_t));
+
+    // Temporary buffer to work with (copy since strtok modifies the string)
+    char cmd_copy[SH_CMD_MAX];
+    strncpy(cmd_copy, cmd_line, SH_CMD_MAX);
+    cmd_copy[SH_CMD_MAX - 1] = '\0'; // Ensure null termination
+
+    // Tokenize the command line by pipes '|'
+    char *cmd = strtok(cmd_copy, PIPE_STRING);
+    while (cmd != NULL)
+    {
+        // Trim leading spaces
+        while (*cmd == SPACE_CHAR) cmd++;
+
+        // Ensure the number of commands does not exceed CMD_MAX
+        if (clist->num >= CMD_MAX)
+        {
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        // Extract executable and arguments
+        command_t *curr_cmd = &clist->commands[clist->num];
+        char *exe = strtok(cmd, " ");
+        if (!exe)
+        {
+            return WARN_NO_CMDS;
+        }
+
+        // Ensure executable size does not exceed EXE_MAX
+        if (strlen(exe) >= EXE_MAX)
+        {
+            return ERR_CMD_OR_ARGS_TOO_BIG;
+        }
+        strcpy(curr_cmd->exe, exe);
+
+        // Extract arguments (everything after the first token)
+        char *args = strtok(NULL, "");
+        if (args)
+        {
+            // Ensure argument size does not exceed ARG_MAX
+            if (strlen(args) >= ARG_MAX)
+            {
+                return ERR_CMD_OR_ARGS_TOO_BIG;
+            }
+            strcpy(curr_cmd->args, args);
+        }
+        else
+        {
+            curr_cmd->args[0] = '\0'; // No arguments case
+        }
+
+        clist->num++;
+        cmd = strtok(NULL, PIPE_STRING);
+    }
+
+    return OK;
 }
