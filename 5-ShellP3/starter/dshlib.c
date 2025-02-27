0f139a41df569d53e7613bc219053f6ba0a4c3be
scott <lsy@gmail.com>
2025-02-27 11:36:39 -0500

    formating

--- 5-ShellP3/starter/dshlib.c
+++ 5-ShellP3/starter/dshlib.c
@@ -151,9 +151,10 @@ int alloc_cmd_buff(cmd_buff_t *cmd_buff)
 int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
 {
     clear_cmd_buff(cmd_buff);
+    
     strncpy(cmd_buff->_cmd_buffer, cmd_line, SH_CMD_MAX - 1);
     cmd_buff->_cmd_buffer[SH_CMD_MAX - 1] = '\0';
-
+    
     char *token = strtok(cmd_buff->_cmd_buffer, " ");
     while (token != NULL)
     {
@@ -202,22 +203,29 @@ int clear_cmd_buff(cmd_buff_t *cmd_buff)
     return OK;
 }
 
+
+
 Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd)
 {
+    // Use match_command to identify the built-in command
     Built_In_Cmds cmd_type = match_command(cmd->argv[0]);
-
+    
     switch (cmd_type)
     {
         case BI_CMD_CD:
+            // If there's an argument provided for cd
             if (cmd->argc > 1)
             {
+                // Change directory using chdir()
                 if (chdir(cmd->argv[1]) != 0)
                 {
+                    // Print error message if chdir fails
                     perror("cd");
                 }
             }
             else
             {
+                // If no argument provided, change to home directory
                 char *home = getenv("HOME");
                 if (home && chdir(home) != 0)
                 {
@@ -225,18 +233,20 @@ Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd)
                 }
             }
             return BI_EXECUTED;
-
+        
         case BI_CMD_EXIT:
             return BI_CMD_EXIT;
-
+            
         case BI_CMD_DRAGON:
             printf("Rawr! 🐉\n");
             return BI_EXECUTED;
-
+            
         default:
             return BI_NOT_BI;
     }
 }
+
+
 
 int free_cmd_list(command_list_t *clist)
 {
@@ -248,31 +258,32 @@ int free_cmd_list(command_list_t *clist)
     return OK;
 }
 
+
 int exec_cmd(cmd_buff_t *cmd)
 {
-    pid_t pid;
-    int status;
+    pid_t pid;  // Process ID
+    int status; // Status of child process
 
-    pid = fork();
+    pid = fork(); // Fork a new process
     if (pid < 0)
     {
-        perror("fork");
+        perror("fork"); // Fork failed
         return ERR_EXEC_CMD;
     }
     else if (pid == 0)
     {
         if (execvp(cmd->argv[0], cmd->argv) < 0)
         {
-            perror("execvp");
+            perror("execvp"); // execvp failed
             exit(ERR_EXEC_CMD);
         }
     }
     else
     {
-        waitpid(pid, &status, 0);
+        waitpid(pid, &status, 0); // Wait for the child process to complete
         if (WIFEXITED(status))
         {
-            return WEXITSTATUS(status);
+            return WEXITSTATUS(status); // Return the exit status of the child
         }
         else
         {
@@ -282,6 +293,7 @@ int exec_cmd(cmd_buff_t *cmd)
     return OK;
 }
 
+
 int execute_pipeline(command_list_t *clist)
 {
     if (clist->num == 0)
@@ -290,9 +302,10 @@ int execute_pipeline(command_list_t *clist)
         return WARN_NO_CMDS;
     }
 
-    int pipes[CMD_MAX - 1][2];
-    pid_t pids[CMD_MAX];
+    int pipes[CMD_MAX - 1][2]; // Pipes for each command in the pipeline
+    pid_t pids[CMD_MAX];       // Child processes
 
+    // Create pipes for each command in the pipeline
     for (int i = 0; i < clist->num - 1; i++)
     {
         if (pipe(pipes[i]) == -1)
@@ -302,6 +315,7 @@ int execute_pipeline(command_list_t *clist)
         }
     }
 
+    // Execute each command in the pipeline
     for (int i = 0; i < clist->num; i++)
     {
         pids[i] = fork();
@@ -309,6 +323,7 @@ int execute_pipeline(command_list_t *clist)
         if (pids[i] == -1)
         {
             perror("fork");
+            // Close all previously created pipes
             for (int j = 0; j < i; j++)
             {
                 close(pipes[j][0]);
@@ -318,7 +333,8 @@ int execute_pipeline(command_list_t *clist)
         }
 
         if (pids[i] == 0)
-        {
+        { // Child process
+            // Redirect input if it's not the first command
             if (i > 0)
             {
                 if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1)
@@ -328,6 +344,7 @@ int execute_pipeline(command_list_t *clist)
                 }
             }
 
+            // Redirect output if it's not the last command
             if (i < clist->num - 1)
             {
                 if (dup2(pipes[i][1], STDOUT_FILENO) == -1)
@@ -337,22 +354,25 @@ int execute_pipeline(command_list_t *clist)
                 }
             }
 
+            // Close all pipes in the child process
             for (int j = 0; j < clist->num - 1; j++)
             {
                 close(pipes[j][0]);
                 close(pipes[j][1]);
             }
 
+            // Check for built-in commands first
             Built_In_Cmds bi_status = exec_built_in_cmd(&clist->commands[i]);
             if (bi_status == BI_EXECUTED)
             {
-                exit(OK);
+                exit(OK); // Exit with success if built-in command was executed
             }
             else if (bi_status == BI_CMD_EXIT)
             {
-                exit(OK_EXIT);
+                exit(OK_EXIT); // Exit with special code for exit command
             }
 
+            // Execute the command
             if (execvp(clist->commands[i].argv[0], clist->commands[i].argv) < 0)
             {
                 perror("execvp");
@@ -361,26 +381,109 @@ int execute_pipeline(command_list_t *clist)
         }
     }
 
+    // Parent process: close all pipe ends
     for (int i = 0; i < clist->num - 1; i++)
     {
         close(pipes[i][0]);
         close(pipes[i][1]);
     }
 
+    // Wait for all child processes to finish
     int exit_status = OK;
     for (int i = 0; i < clist->num; i++)
     {
         int status;
         waitpid(pids[i], &status, 0);
-        if (WIFEXITED(status))
+        
+        // Check if any child exited with OK_EXIT status
+        if (WIFEXITED(status) && WEXITSTATUS(status) == OK_EXIT)
         {
-            exit_status = WEXITSTATUS(status);
-        }
-        else
-        {
-            exit_status = ERR_EXEC_CMD;
+            exit_status = OK_EXIT;
         }
     }
 
     return exit_status;
 }
+
+
+int build_cmd_list(char *cmd_line, command_list_t *clist)
+{
+    // Initialize command list
+    clist->num = 0;
+    
+    // Make a copy of cmd_line to avoid modifying the original
+    char *cmd_copy = strdup(cmd_line);
+    if (cmd_copy == NULL)
+    {
+        return ERR_MEMORY;
+    }
+    
+    char *saveptr;
+    // Split the command line by pipe character
+    char *token = strtok_r(cmd_copy, "|", &saveptr);
+    while (token != NULL && clist->num < CMD_MAX)
+    {
+        // Create a temporary working copy of the token
+        char *tmp = strdup(token);
+        if (tmp == NULL)
+        {
+            free(cmd_copy);
+            return ERR_MEMORY;
+        }
+        
+        // Trim leading and trailing spaces
+        char *start = tmp;
+        while (*start && isspace(*start)) start++;
+        
+        char *end = start + strlen(start) - 1;
+        while (end > start && isspace(*end)) *end-- = '\0';
+        
+        // Skip if empty after trimming
+        if (*start == '\0')
+        {
+            free(tmp);
+            token = strtok_r(NULL, "|", &saveptr);
+            continue;
+        }
+        
+        // Allocate memory for the command buffer
+        if (alloc_cmd_buff(&clist->commands[clist->num]) != OK)
+        {
+            free(tmp);
+            free(cmd_copy);
+            return ERR_MEMORY;
+        }
+        
+        // Build the command buffer
+        if (build_cmd_buff(start, &clist->commands[clist->num]) != OK)
+        {
+            free(tmp);
+            free(cmd_copy);
+            return ERR_CMD_OR_ARGS_TOO_BIG;
+        }
+        
+        clist->num++;
+        free(tmp);
+        token = strtok_r(NULL, "|", &saveptr);
+    }
+    
+    free(cmd_copy);
+    
+    // Check if we have too many commands
+    if (token != NULL && clist->num >= CMD_MAX)
+    {
+        return ERR_TOO_MANY_COMMANDS;
+    }
+    
+    return OK;
+}
+
+
+
+
+
+
+
+
+
+
